1ebd7c7fec80d2284204f7a3f95fbfbdea08decb
Isaac No <isaacyno@gmail.com>
2023-03-24 02:54:01 -0700

    connected csv to html

--- red_herring-test/.ipynb_checkpoints/app-checkpoint.py
+++ red_herring-test/.ipynb_checkpoints/app-checkpoint.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import random
 
 app = Flask(__name__)
 
+# create df
+df = pd.read_csv("categories.csv")
+df_len = len(df)
+
+
 @app.route('/')
 def index():
-    words = [
-        'Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grape', 'Honeydew',
-        'Iceberg', 'Jicama', 'Kale', 'Lettuce', 'Mushroom', 'Napa', 'Onion', 'Pepper'
-    ]
-    return render_template('index.html', words=words)
+    # get 7 random rows from the df, the first 3 will be for the 3 categories, the last 4 for red herrings
+    my_rows = random.sample(range(df_len), 7)
+    L = []
+    # add the category lists
+    for i in my_rows[:3]:
+        L.append(list(df.iloc[i, random.sample(range(10), 4)]))
+    # red herring list
+    rh = []
+    for j in my_rows[3:]:
+        rh.append(df.iloc[j, random.sample(range(10), 1)].item())
+    L.append(rh)
+    return render_template('index.html', words=L)
 
 @app.route('/submit', methods=['POST'])
 def submit():
