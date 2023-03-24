fd23ed93ff8af6d3c00995e36343bd5c9e31a1bd
Isaac No <isaacyno@gmail.com>
2023-03-24 03:20:08 -0700

    debug randomization

--- red_herring-test/app.py
+++ red_herring-test/app.py
@@ -6,23 +6,42 @@ app = Flask(__name__)
 
 # create df
 df = pd.read_csv("categories.csv")
+df = df.drop("Unnamed: 0", axis=1)
+df = df.reset_index(drop=True)
 df_len = len(df)
 
 
 @app.route('/')
 def index():
-    # get 7 random rows from the df, the first 3 will be for the 3 categories, the last 4 for red herrings
-    my_rows = random.sample(range(df_len), 7)
-    L = []
-    # add the category lists
-    for i in my_rows[:3]:
-        L.append(list(df.iloc[i, random.sample(range(10), 4)]))
-    # red herring list
-    rh = []
-    for j in my_rows[3:]:
-        rh.append(df.iloc[j, random.sample(range(10), 1)].item())
-    L.append(rh)
-    return render_template('index.html', words=L)
+    #pick 3 random categories
+    allDiff = True
+    
+    while allDiff:
+        allwords = random.sample(range(df_len), 7)
+        cats = allwords[:3]
+        diff = allwords[3:]
+        solution = []
+        #pick 4 random words per category
+        count = 0
+        for n in cats:
+            catn = df.iloc[n]
+            randomindex = random.sample(range(10), 4)
+            #create solution
+            solution.append(catn[randomindex].to_list())
+        #pick 4 random different words from different topics as red herrings
+        rh = []
+        for k in diff:
+            redn = df.iloc[k]
+            randomindex = random.sample(range(10), 1)
+            rh.append(redn[randomindex].item())
+        solution.append(rh)
+        truewords = [item for sublist in solution for item in sublist]
+        #make sure all the words are unique
+        if len(truewords) == len(set(truewords)):
+            allDiff = False
+    #randomize list of words
+    random.shuffle(truewords)
+    return render_template('index.html', words=truewords)
 
 @app.route('/submit', methods=['POST'])
 def submit():
